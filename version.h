19b33985c443fc001650c2721a75c455ce4194d3
Tomomasa UCHIDA <tomomasa.is.0930@gmail.com>
2019-03-30 13:03:47 +0900

    initial commit

---
+++ version.h
@@ -0,0 +1,29 @@
+/////////////////////
+///// version.h /////
+/////////////////////
+
+#if !defined  VERSION_HH
+#define       VERSION_HH
+
+
+//----- TITLE of SPBR and OPBR 
+const char SPBR_APP_TITLE    [] = "===== Stochastic Point-based Renderer (SPBR) =====";
+const char OPBR_APP_TITLE    [] = "===== Opaque Point-based Renderer (OPBR) =====";
+
+//----- VERSION of SPBR and OPBR
+const char SOPBR_VERSION [] = "Version beta-16b";
+
+//----- DATE
+const char DATE          [] = "2019/1/30 (ARC Shrewd Meeting Version)";
+
+//----- AUTHOR and INSTITUTE
+const char AUTHOR       [] = "      Satoshi Tanaka and Kyoko Hasegawa";
+const char INSTITUTE    [] = "           Ritsumeikan University";
+
+//----- WINDOW TITLE
+const char SPBR_WINDOW_TITLE   []   = "SPBR version beta-16b";
+const char OPBR_WINDOW_TITLE   []   = "OPBR version beta-16b";
+
+#endif 
+// end of version.h
+
