1bc35f52be14ae4f6b5bea7156f49283fd98adad
Tomomasa UCHIDA <tomomasa.is.0930@gmail.com>
2019-04-01 22:01:32 +0900

    update

--- main.cpp
+++ main.cpp
@@ -1,18 +1,18 @@
 ////////////////////////////////////
-///// main.cpp (spbr_main.cpp) /////
+///// main.cpp (opbr_main.cpp) /////
 ////////////////////////////////////
 
 
 #include "spbr.h"
 #include "version.h"
-#include "display_spbr_usage.h"
+#include "display_opbr_usage.h"
 #include "mainfn_utility.h"
 
 // Prototype declaration
-int  mainsub_spbr_spbrbin( int argc, char** argv );
-int  mainsub_spbr_plybin(int argc, char** argv);//2018/8
-int  mainsub_spbr_plyascii ( int argc, char** argv );
-int  mainsub_spbr_spbrascii ( int argc, char** argv );
+int  mainsub_opbr_spbrbin( int argc, char** argv );
+int  mainsub_opbr_plybin(int argc, char** argv);//2018/8
+int  mainsub_opbr_plyascii ( int argc, char** argv );
+int  mainsub_opbr_spbrascii ( int argc, char** argv );
 
 // MACRO
 //#define DEBUG_PLY
@@ -20,13 +20,13 @@ int  mainsub_spbr_spbrascii ( int argc, char** argv );
 //-----
 int main(int argc, char** argv) {
   // Opening Message and Usage
-  displaySPBR_OpeningMessage();
-  displaySPBR_Usage();
+  displayOPBR_OpeningMessage();
+  displayOPBR_Usage();
 
   // Help 
   if (argc >= 2 && !strcmp(argv[1], "-h")) {
-    displaySPBRCommands(std::cout);
-    displaySPBRDataFormat(std::cout);
+    displayOPBRCommands(std::cout);
+    displayOPBRDataFormat(std::cout);
     exit(1);
   }
   else if (argc < 2) {
@@ -43,25 +43,26 @@ int main(int argc, char** argv) {
   if (isBinarySPBR_File(argv[1])) {
     // binary SPBR file 
     //    std::cout << "** Binary SPBR file: " << argv[1] << std::endl;
-    mainsub_spbr_spbrbin(argc, argv);
+    mainsub_opbr_spbrbin (argc, argv);
   }
-  else if (isBINARY_PLY_File(argv[1])) {
+  else if (isBINARY_PLY_File(argv[1])){
     // binary PLY file
     //    std::cout << "** Binary PLY file: "  << argv[1] << std::endl;
-    mainsub_spbr_plybin(argc, argv);
-  } 
-  else if (isASCII_PLY_File(argv[1])) {//TANAKA 2017/11/05
+    mainsub_opbr_plybin(argc, argv);
+  }
+  else  if (isASCII_PLY_File(argv[1])) {//TANAKA 2017/11/05
     // ASCII PLY file
     //    std::cout << "** ASCII PLY file: "   << argv[1] << std::endl;
-    mainsub_spbr_plyascii(argc, argv);
+    mainsub_opbr_plyascii(argc, argv);
   }
   else {
     // ASCII SPBR file
     //    std::cout << "** ASCII SPBR file: "  << argv[1] <<std::endl;
-    mainsub_spbr_spbrascii(argc, argv);
+    mainsub_opbr_spbrascii( argc, argv );
   }
 
   //EndFn
   return 0;
 
-}// main() for SPBR
+}
+// main() for OPBR
